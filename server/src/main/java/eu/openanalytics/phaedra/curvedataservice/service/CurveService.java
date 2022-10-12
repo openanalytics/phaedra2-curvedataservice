a5526276b89e61907929cbd0aece533d7a64153a
sberberovic <sasa.berberovic@openanalytics.eu>
2022-10-12 10:35:43 +0200

    Update CurveService createCurve method

--- server/src/main/java/eu/openanalytics/phaedra/curvedataservice/service/CurveService.java
+++ server/src/main/java/eu/openanalytics/phaedra/curvedataservice/service/CurveService.java
@@ -23,20 +23,41 @@ package eu.openanalytics.phaedra.curvedataservice.service;
 import eu.openanalytics.curvedataservice.dto.CurveDTO;
 import eu.openanalytics.phaedra.curvedataservice.model.Curve;
 import eu.openanalytics.phaedra.curvedataservice.repository.CurveRepository;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Service;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
 
 @Service
 public class CurveService {
     private final ModelMapper modelMapper;
     private final CurveRepository curveRepository;
 
-    public CurveService(ModelMapper modelMapper, CurveRepository curveRepository) {
+    private final DataSource dataSource;
+
+    public CurveService(ModelMapper modelMapper, CurveRepository curveRepository, DataSource dataSource) {
         this.modelMapper = modelMapper;
         this.curveRepository = curveRepository;
+        this.dataSource = dataSource;
     }
 
     public CurveDTO createCurve(CurveDTO curveDTO) {
-        Curve savedCurve = curveRepository.save(modelMapper.map(curveDTO));
-        return modelMapper.map(savedCurve);
+        // workaround for https://github.com/spring-projects/spring-data-jdbc/issues/1033
+        var simpleJdbcInsert = new SimpleJdbcInsert(dataSource).withTableName("curve").usingGeneratedKeyColumns("id");
+        Number id = simpleJdbcInsert.executeAndReturnKey(new HashMap<>() {{
+            put("substance_name", curveDTO.getSubstanceName());
+            put("plate_id", curveDTO.getPlateId());
+            put("feature_id", curveDTO.getFeatureId());
+            put("protocol_id", curveDTO.getProtocolId());
+            put("result_set_id", curveDTO.getResultSetId());
+            put("fit_date", curveDTO.getFitDate());
+            put("version", curveDTO.getVersion());
+            put("x_axis_labels", curveDTO.getXAxisLabels());
+            put("plot_dose_data", curveDTO.getPlotDoseData());
+            put("plot_prediction_data", curveDTO.getPlotPredictionData());
+        }});
+
+        return modelMapper.map(curveRepository.findById(id.longValue()).get());
     }
 }
