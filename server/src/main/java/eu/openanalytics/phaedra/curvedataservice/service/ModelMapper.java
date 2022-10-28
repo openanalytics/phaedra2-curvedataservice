10076de970a6a4287e7c482b27ced28fc46de5fb
sberberovic <sasa.berberovic@openanalytics.eu>
2022-10-28 12:29:37 +0200

    Update ModelMapper

--- server/src/main/java/eu/openanalytics/phaedra/curvedataservice/service/ModelMapper.java
+++ server/src/main/java/eu/openanalytics/phaedra/curvedataservice/service/ModelMapper.java
@@ -22,10 +22,15 @@ package eu.openanalytics.phaedra.curvedataservice.service;
 
 import eu.openanalytics.curvedataservice.dto.CurveDTO;
 import eu.openanalytics.phaedra.curvedataservice.model.Curve;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.modelmapper.config.Configuration;
 import org.modelmapper.convention.NameTransformers;
 import org.modelmapper.convention.NamingConventions;
 import org.springframework.stereotype.Service;
+
+import static java.lang.Float.NaN;
+import static java.lang.Float.parseFloat;
+import static org.apache.commons.lang3.math.NumberUtils.isCreatable;
 
 @Service
 public class ModelMapper {
@@ -57,6 +62,24 @@ public class ModelMapper {
         curve.setWells(curveDTO.getWells());
         curve.setFeatureValues(curveDTO.getFeatureValues());
         curve.setWellConcentrations(curveDTO.getWellConcentrations());
+        curve.setWeights(curveDTO.getWeights());
+        curve.setPIC50(NumberUtils.isCreatable(curveDTO.getPIC50()) ? Float.parseFloat(curveDTO.getPIC50()) : Float.NaN);
+        curve.setPIC50Censor(curveDTO.getPIC50Censor());
+        curve.setPIC50StdErr(curveDTO.getPIC50StdErr());
+        curve.setEMax(curveDTO.getEMax());
+        curve.setEMin(curveDTO.getEMin());
+        curve.setEMaxConc(curveDTO.getEMaxConc());
+        curve.setEMinConc(curveDTO.getEMinConc());
+        curve.setPIC20(curveDTO.getPIC20());
+        curve.setPIC80(curveDTO.getPIC80());
+        curve.setSlope(curveDTO.getSlope());
+        curve.setBottom(curveDTO.getBottom());
+        curve.setTop(curveDTO.getTop());
+        curve.setSlopeLowerCI(curveDTO.getSlopeLowerCI());
+        curve.setSlopeUpperCI(curveDTO.getSlopeUpperCI());
+        curve.setResidualVariance(curveDTO.getResidualVariance());
+        curve.setWarning(curveDTO.getWarning());
+
         return curve;
     }
 
@@ -79,6 +102,22 @@ public class ModelMapper {
                 .wells(curve.getWells())
                 .featureValues(curve.getFeatureValues())
                 .wellConcentrations(curve.getWellConcentrations())
+                .weights(curve.getWeights())
+                .pIC50(String.valueOf(curve.getPIC50()))
+                .pIC50StdErr(curve.getPIC50StdErr())
+                .eMax(curve.getEMax())
+                .eMin(curve.getEMin())
+                .eMaxConc(curve.getEMaxConc())
+                .eMinConc(curve.getEMinConc())
+                .pIC20(curve.getPIC20())
+                .pIC80(curve.getPIC80())
+                .slope(curve.getSlope())
+                .bottom(curve.getBottom())
+                .top(curve.getTop())
+                .slopeLowerCI(curve.getSlopeLowerCI())
+                .slopeUpperCI(curve.getSlopeUpperCI())
+                .residualVariance(curve.getResidualVariance())
+                .warning(curve.getWarning())
                 .build();
     }
 }
